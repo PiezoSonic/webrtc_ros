9710186c8f3cc061e04cadfd85085c394fe509df
羽部　美樹 <mkh@lares.dti.ne.jp>
2025-06-10 15:21:32 +0900

    ROSパラメータの扱いが間違っていた（ROS 1のままで機能していなかった）ので、修正 (#4)
    Co-authored-by: HABU Miki <m.habu@piezo-sonic.com>

--- webrtc_ros/src/webrtc_ros_server.cpp
+++ webrtc_ros/src/webrtc_ros_server.cpp
@@ -5,88 +5,93 @@
 namespace webrtc_ros
 {
 
-MessageHandler* WebrtcRosServer_handle_new_signaling_channel(void* _this, SignalingChannel *channel)
-{
-    MessageHandler* result;
-    ((WebrtcRosServer*) _this)->signaling_thread_->BlockingCall( 
-        [&]{ result = ((WebrtcRosServer*) _this)->handle_new_signaling_channel(channel);}
-    );
+  MessageHandler *WebrtcRosServer_handle_new_signaling_channel(void *_this, SignalingChannel *channel)
+  {
+    MessageHandler *result;
+    ((WebrtcRosServer *)_this)->signaling_thread_->BlockingCall([&]
+                                                                { result = ((WebrtcRosServer *)_this)->handle_new_signaling_channel(channel); });
     return result;
-}
-
-WebrtcRosServer::WebrtcRosServer(rclcpp::Node::SharedPtr nh)
-  : nh_(nh), itf_(nh, std::make_shared<image_transport::ImageTransport>(nh))
-{
-  rtc::InitializeSSL();
-
-  int port;
-  nh_->get_parameter_or<int>("port", port, 8080);
-  nh_->get_parameter_or<std::string>("image_transport", image_transport_, std::string("raw"));
-
-  signaling_thread_ = rtc::Thread::CreateWithSocketServer();
-  signaling_thread_->Start();
-  server_.reset(WebrtcWebServer::create(nh_, port, &WebrtcRosServer_handle_new_signaling_channel, this));
-}
-
-void WebrtcRosServer::cleanupWebrtcClient(WebrtcClient *client) {
-  {
-    std::unique_lock<std::mutex> lock(clients_mutex_);
-    clients_.erase(client);
-    delete client; // delete while holding the lock so that we do not exit before were done
   }
-  shutdown_cv_.notify_all();
-}
 
-MessageHandler* WebrtcRosServer::handle_new_signaling_channel(SignalingChannel *channel)
-{
-  auto client = std::make_shared<WebrtcClient>(nh_, itf_, image_transport_, channel);
-
-	// TODO: Handle cleanup std::bind(&WebrtcRosServer::cleanupWebrtcClient, this, std::placeholders::_1));
-  // hold a shared ptr until the object is initialized (holds a ptr to itself)
-  client->init(client);
+  WebrtcRosServer::WebrtcRosServer(rclcpp::Node::SharedPtr nh)
+      : nh_(nh), itf_(nh, std::make_shared<image_transport::ImageTransport>(nh))
   {
-    std::unique_lock<std::mutex> lock(clients_mutex_);
-    clients_[client.get()] = WebrtcClientWeakPtr(client);
+    rtc::InitializeSSL();
+
+    nh_->declare_parameter<std::string>("image_transport", "raw");
+    nh_->get_parameter("image_transport", image_transport_);
+    int port;
+    nh_->declare_parameter<int>("port", 8080);
+    port = nh_->get_parameter("port").as_int();
+
+    signaling_thread_ = rtc::Thread::CreateWithSocketServer();
+    signaling_thread_->Start();
+    server_.reset(WebrtcWebServer::create(nh_, port, &WebrtcRosServer_handle_new_signaling_channel, this));
   }
-  return client->createMessageHandler();
-}
 
-WebrtcRosServer::~WebrtcRosServer()
-{
-  stop();
-
-  // Send all clients messages to shutdown, cannot call dispose of share ptr while holding clients_mutex_
-  // It will deadlock if it is the last shared_ptr because it will try to remove it from the client list
-  std::vector<WebrtcClientWeakPtr> to_invalidate;
+  void WebrtcRosServer::cleanupWebrtcClient(WebrtcClient *client)
   {
-    std::unique_lock<std::mutex> lock(clients_mutex_);
-    for(auto& client_entry : clients_) {
-      to_invalidate.push_back(client_entry.second);
+    {
+      std::unique_lock<std::mutex> lock(clients_mutex_);
+      clients_.erase(client);
+      delete client; // delete while holding the lock so that we do not exit before were done
     }
-  }
-  for(WebrtcClientWeakPtr& client_weak : to_invalidate) {
-    std::shared_ptr<WebrtcClient> client = client_weak.lock();
-    if (client)
-      client->invalidate();
+    shutdown_cv_.notify_all();
   }
 
-  // Wait for all our clients to shown
+  MessageHandler *WebrtcRosServer::handle_new_signaling_channel(SignalingChannel *channel)
   {
-    std::unique_lock<std::mutex> lock(clients_mutex_);
-    shutdown_cv_.wait(lock, [this]{ return this->clients_.size() == 0; });
+    auto client = std::make_shared<WebrtcClient>(nh_, itf_, image_transport_, channel);
+
+    // TODO: Handle cleanup std::bind(&WebrtcRosServer::cleanupWebrtcClient, this, std::placeholders::_1));
+    // hold a shared ptr until the object is initialized (holds a ptr to itself)
+    client->init(client);
+    {
+      std::unique_lock<std::mutex> lock(clients_mutex_);
+      clients_[client.get()] = WebrtcClientWeakPtr(client);
+    }
+    return client->createMessageHandler();
   }
 
-  rtc::CleanupSSL();
-}
+  WebrtcRosServer::~WebrtcRosServer()
+  {
+    stop();
 
-void WebrtcRosServer::run()
-{
-  server_->run();
-}
+    // Send all clients messages to shutdown, cannot call dispose of share ptr while holding clients_mutex_
+    // It will deadlock if it is the last shared_ptr because it will try to remove it from the client list
+    std::vector<WebrtcClientWeakPtr> to_invalidate;
+    {
+      std::unique_lock<std::mutex> lock(clients_mutex_);
+      for (auto &client_entry : clients_)
+      {
+        to_invalidate.push_back(client_entry.second);
+      }
+    }
+    for (WebrtcClientWeakPtr &client_weak : to_invalidate)
+    {
+      std::shared_ptr<WebrtcClient> client = client_weak.lock();
+      if (client)
+        client->invalidate();
+    }
 
-void WebrtcRosServer::stop()
-{
-  server_->stop();
-}
+    // Wait for all our clients to shown
+    {
+      std::unique_lock<std::mutex> lock(clients_mutex_);
+      shutdown_cv_.wait(lock, [this]
+                        { return this->clients_.size() == 0; });
+    }
+
+    rtc::CleanupSSL();
+  }
+
+  void WebrtcRosServer::run()
+  {
+    server_->run();
+  }
+
+  void WebrtcRosServer::stop()
+  {
+    server_->stop();
+  }
 
 }
